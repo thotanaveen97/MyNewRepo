cb076ed6e5c815cb9e2f3497e78d2e561ebccc0b
root <root@ip-172-31-28-210.ec2.internal>
2025-11-06 09:11:58 +0000

    now i am adding data

--- app.java
+++ app.java
@@ -5,9 +5,11 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
     }
 }
+
 //develop-2 push from local
 public class HelloWorld {
     public static void main(String[] args) {
         // Write your code here
     }
 }
+new code by dev-1 without pull the code
