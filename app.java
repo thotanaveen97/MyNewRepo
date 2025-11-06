cad237483c7feb89e1e28998a75c85ef8dbe8d7c
thotanaveen97 <thotanaveen688@gmail.com>
2025-11-06 14:24:52 +0530

    Update app.java

--- app.java
+++ app.java
@@ -5,3 +5,9 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
     }
 }
+//develop-2 push from local
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here
+    }
+}
